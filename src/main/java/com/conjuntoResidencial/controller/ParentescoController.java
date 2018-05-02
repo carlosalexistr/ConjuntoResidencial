e08251d2904c58599f41d4a6f4f434050e0b03ca
daQuin <jesusdanielqs@ufps.edu.co>
2018-05-02 11:06:49 -0500

    daQuin Parentesco

---
+++ src/main/java/com/conjuntoResidencial/controller/ParentescoController.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.conjuntoResidencial.controller;
+
+import com.conjuntoResidencial.dao.ParentescoDAO.ParentescoDAOImp;
+import com.conjuntoResidencial.model.Parentesco;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "ParentescoController", urlPatterns = {"/ParentescoController"})
+public class ParentescoController extends HttpServlet {
+
+    ParentescoDAOImp p = new ParentescoDAOImp();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ParentescoController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ParentescoController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    public void mostrarRegistros(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Parentesco> parentesco = p.findAll();
+
+        request.setAttribute("parentesco", parentesco);
+        request.getRequestDispatcher("/Parentesco.jsp").forward(request, response);
+    }
+
+    public void registrarParentesco(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String descripcion = request.getParameter("descripcion");
+        Parentesco pa = new Parentesco();
+        pa.setId(id);
+        pa.setDescripcion(descripcion);
+        p.save(pa);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        mostrarRegistros(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        registrarParentesco(request, response);
+        mostrarRegistros(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/Parentesco.jsp
@@ -0,0 +1,57 @@
+<%-- 
+    Document   : Parentesco
+    Created on : 2/05/2018, 09:09:58 AM
+    Author     : DELL
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Parentesco</h1>
+
+        <form action="ParentescoController" method="post">
+            <ul>
+                <li><label>Id</label><input type="text" name="id">
+                </li>
+                <li><label>Descripcion</label><input type="text" name="descripcion">
+                </li>
+                <br>
+                <li><input type="submit" value="registrar" name=""> </li>
+            </ul>
+        </form>
+        
+          <table>
+            <thead>
+                <tr>
+                    <th>
+                        Id
+                    </th>
+                    <th>
+                        Descripcion
+                    </th>                   
+                </tr>
+            </thead>
+            <tbody>
+                <c:forEach items="${parentesco}" var="parentesco">
+                <tr>
+                <td>
+                    ${parentesco.getId()}
+                </td>
+                <td>
+                    ${parentesco.getDescripcion()}
+                </td>
+                </tr>
+            </c:forEach>
+        </tbody>
+
+    </table>
+
+
+</body>
+</html>
